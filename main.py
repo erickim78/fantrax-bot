427823a17a15230053f83b2d3412309deb7c58f8
Eric Kim <erick@aitmed.com>
2024-10-20 22:41:29 -0700

    boilerplate done

--- main.py
+++ main.py
@@ -13,20 +13,30 @@ from discord import app_commands
 class MyBot(commands.Bot):
     #Init
     def __init__(self):
-        return
-
+        myIntents = discord.Intents.default()
+        super().__init__(
+            command_prefix = "!",
+            intents = myIntents,
+            application_id = config.clientID
+        )
 
 
     # Setup hook
     async def setup_hook(self):
-        return
-        # None yet
-        # await load_extensions()
+        await load_cogs()
 
 
     # On ready
     async def on_ready(self):
         print(f'Logged in as {self.user}')
+
+
+# Load Cogs
+async def load_cogs():
+    for filename in os.listdir('./cogs'):
+        if filename.endswith('.py'):
+            await bot.load_extension(f'cogs.{filename[:-3]}')
+            print(f'Loaded {filename} Cog')
 
 
 
